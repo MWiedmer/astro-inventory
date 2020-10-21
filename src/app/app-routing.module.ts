c6762956fe6c0dff8e566ce1b42e609bdc510bef
Markus Wiedmer <ovi@gmx.ch>
2020-10-21 22:28:20 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { OcularsComponent } from './oculars/oculars.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'oculars', component: OcularsComponent },
+	{ path: 'home', component: LandingpageComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
